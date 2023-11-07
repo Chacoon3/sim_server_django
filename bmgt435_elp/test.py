6bd5abfca318cd7495cbe5b3504ce4a546dad7b5
Chacoon3 <dyzily@foxmail.com>
2023-11-07 16:46:07 -0500

    upd test case

--- bmgt435_elp/test.py
+++ bmgt435_elp/test.py
@@ -2,26 +2,25 @@ from django.test import  TestCase
 from .bmgtModels import *
 from .apis import *
 from django.contrib.auth.hashers import make_password, check_password
-from requests import Request, Response
 from django.test import Client
 import json 
 
-class AppTest(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
+class AppAuthTest(TestCase):
+
+    def setUp(self):
         BMGTUser.objects.create(first_name='first', last_name='last', did='did', role='admin').save()
         BMGTUser.objects.create(first_name='first321', last_name='last232', did='did232', role='user').save()
 
 
-    def testBmgtUser(self):
+    def queryBmgtUser(self):
         user1 = BMGTUser.objects.get(first_name='first')
         user2 = BMGTUser.objects.get(first_name='first321')
         self.assertEqual(user1.first_name, 'first')
         self.assertEqual(user2.first_name, 'first321')
 
 
-    def testSignUpPositive(self):
+    def signUpPositive(self):
         client = Client()
         resp = client.post(
             '/bmgt435-service/api/auth/sign-up', 
@@ -31,7 +30,7 @@ class AppTest(TestCase):
         self.assertEqual(resp.status_code, 200)
 
 
-    def testSignUpNegative(self):
+    def signUpNegative(self):
         resp = Client().post(
             '/bmgt435-service/api/auth/sign-up',
             json.dumps({'did':'', 'password':""}),
@@ -40,7 +39,16 @@ class AppTest(TestCase):
         self.assertNotEqual(resp.status_code, 200)
 
 
-    def testRepeatedSignUp(self):
+    def signUpNonExistentUser(self):
+        resp = Client().post(
+            '/bmgt435-service/api/auth/sign-up',
+            json.dumps({'did':'did323', 'password':"pass32132321.$"}),
+            'application/json'
+        )
+        self.assertNotEqual(resp.status_code, 200)
+
+
+    def repeatedSignUp(self):
         client = Client()
         resp = client.post(
             '/bmgt435-service/api/auth/sign-up', 
@@ -52,5 +60,35 @@ class AppTest(TestCase):
             '/bmgt435-service/api/auth/sign-up',
             json.dumps({'did': 'did', 'password': 'pa3232.ssword'}),
             'application/json'
+        )
+        self.assertNotEqual(resp.status_code, 200)
+
+    
+    def signInPositive(self):
+        c = Client()
+        c.post(
+            'bmgt435-service/api/auth/sign-up',
+            json.dumps({'did':'did', 'password':'pa3232.ssword'}),
+        )
+
+        resp = c.post(
+            'bmgt435-service/api/auth/sign-in',
+            json.dumps({'did': 'did', 'password': 'pa3232.ssword'}),
+            'application/json'
+        )
+
+        self.assertEqual(resp.status_code, 200)
+
+
+    def signInNegative(self):
+        c = Client()
+        c.post(
+            'bmgt435-service/api/auth/sign-up',
+            json.dumps({'did':'did', 'password':'pa3232.ssword'}),
+        )
+
+        resp = c.post(
+            'bmg435-service/api/auth/sign-in',
+            json.dumps({'did':'did', 'password':'pa3232.ssw2ord'}),
         )
         self.assertNotEqual(resp.status_code, 200)
