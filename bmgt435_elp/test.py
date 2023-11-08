a6768e36607f749e4e51a1e436935db6e8b24670
Chacoon3 <dyzily@foxmail.com>
2023-11-07 22:56:35 -0500

    upd

--- bmgt435_elp/test.py
+++ bmgt435_elp/test.py
@@ -4,21 +4,43 @@ from .apis import *
 import json
 
 
+def _clientSetCookies(client:Client, cookies:dict):
+    for k, v in cookies.items():
+        client.cookies[k] = v
+
+
+def _clientSignUp(client:Client, did:str, password:str):
+    return client.post(
+        'bmgt435-service/api/auth/sign-up',
+        json.dumps({'did': did, 'password': password}),
+        'application/json'
+    )
+
+
+def _clientSignIn(client:Client, did:str, password:str):
+    resp = client.post(
+        'bmgt435-service/api/auth/sign-in',
+        json.dumps({'did': did, 'password': password}),
+        'application/json'
+    )
+    return resp
+
+
 class AppAuthTest(TestCase):
 
     def setUp(self):
-        BMGTUser.objects.create(first_name='first', last_name='last', did='did', role='admin').save()
-        BMGTUser.objects.create(first_name='first321', last_name='last232', did='did232', role='user').save()
+        BMGTUser(first_name='first', last_name='last', did='did', role='admin').save()
+        BMGTUser(first_name='first321', last_name='last232', did='did232', role='user').save()
 
 
-    def queryBmgtUser(self):
+    def testQueryBmgtUser(self):
         user1 = BMGTUser.objects.get(first_name='first')
         user2 = BMGTUser.objects.get(first_name='first321')
         self.assertEqual(user1.first_name, 'first')
         self.assertEqual(user2.first_name, 'first321')
 
 
-    def signUpPositive(self):
+    def testSignUpPositive(self):
         client = Client()
         resp = client.post(
             '/bmgt435-service/api/auth/sign-up', 
@@ -28,7 +50,7 @@ class AppAuthTest(TestCase):
         self.assertEqual(resp.status_code, 200)
 
 
-    def signUpNegative(self):
+    def testSignUpNegative(self):
         resp = Client().post(
             '/bmgt435-service/api/auth/sign-up',
             json.dumps({'did':'', 'password':""}),
@@ -37,7 +59,7 @@ class AppAuthTest(TestCase):
         self.assertNotEqual(resp.status_code, 200)
 
 
-    def signUpNonExistentUser(self):
+    def testSignUpNonExistentUser(self):
         resp = Client().post(
             '/bmgt435-service/api/auth/sign-up',
             json.dumps({'did':'did323', 'password':"pass32132321.$"}),
@@ -46,7 +68,7 @@ class AppAuthTest(TestCase):
         self.assertNotEqual(resp.status_code, 200)
 
 
-    def repeatedSignUp(self):
+    def testRepeatedSignUp(self):
         client = Client()
         resp = client.post(
             '/bmgt435-service/api/auth/sign-up', 
@@ -62,12 +84,9 @@ class AppAuthTest(TestCase):
         self.assertNotEqual(resp.status_code, 200)
 
     
-    def signInPositive(self):
+    def testSignInPositive(self):
         c = Client()
-        c.post(
-            'bmgt435-service/api/auth/sign-up',
-            json.dumps({'did':'did', 'password':'pa3232.ssword'}),
-        )
+        _clientSignUp(c, 'did', 'pa3232.ssword')
 
         resp = c.post(
             'bmgt435-service/api/auth/sign-in',
@@ -78,16 +97,18 @@ class AppAuthTest(TestCase):
         self.assertEqual(resp.status_code, 200)
 
 
-    def signInNegative(self):
+    def testSignInNegative(self):
         c = Client()
         c.post(
             'bmgt435-service/api/auth/sign-up',
             json.dumps({'did':'did', 'password':'pa3232.ssword'}),
+            'application/json'
         )
 
         resp = c.post(
             'bmg435-service/api/auth/sign-in',
             json.dumps({'did':'did', 'password':'pa3232.ssw2ord'}),
+            'application/json'
         )
         self.assertNotEqual(resp.status_code, 200)
 
@@ -95,37 +116,59 @@ class AppAuthTest(TestCase):
 class UserApiTest(TestCase):
 
     def setUp(self):
-        BMGTUser.objects.create(first_name='f', last_name='l', did='did', role='admin', activated=1, password='Grave11.').save()
-        BMGTUser.objects.create(first_name='f', last_name='l', did='did323', role='admin', activated=0, password='Grave11.').save()
-        BMGTUser.objects.create(first_name='first321', last_name='last232', did='did232', role='user', activated=1, password='Grave11.').save()
+        BMGTUser(first_name='f', last_name='l', did='did', role='admin', activated=1, password='Grave11.').save()
+        BMGTUser(first_name='f', last_name='l', did='did323', role='admin', activated=0, password='Grave11.').save()
+        BMGTUser(first_name='first321', last_name='last232', did='did232', role='user', activated=1, password='Grave11.').save()
 
     
-    def userMePositive(self):
+    def testUserMePositive(self):
         resp = Client().get(
             'bmgt435-service/api/user/me',
-            json.dumps({'id':1})
-        )
-        self.assertEqual(resp.status_code, 200)
-
-
-    def userMeNegative(self):
-        resp = Client().get(
-            'bmgt435-service/api/user/me',
-            json.dumps({'id':-1})
         )
         self.assertNotEqual(resp.status_code, 200)
 
 
-    def userMeNotActivated(self):
+    def testUserMeAfterSignIn(self):
+        c = Client()
+        _clientSignUp(c, 'did', 'Grave11.')
+        _clientSignIn(c, 'did', 'Grave11.')
+        
+        resp = c.get(
+            'bmgt435-service/api/users/me',
+        )
+        self.assertEqual(resp.status_code, 200)
+
+
+    def testUserMeNegative(self):
         resp = Client().get(
-            'bmgt435-service/api/user/me',
-            json.dumps({'id':2})
+            'bmgt435-service/api/users/me',
+        )
+        self.assertNotEqual(resp.status_code, 200)
+
+
+    def testUserMeNotActivated(self):
+        resp = Client().get(
+            'bmgt435-service/api/users/me',
         )
         self.assertNotEqual(resp.status_code, 200)
 
 
 class GroupApiTest(TestCase):
+
     def setUp(self) -> None:
-        BMGTUser.objects.create(first_name='f', last_name='l', did='did', role='admin', activated=1, password='Grave11.').save()
-        BMGTUser.objects.create(first_name='f', last_name='l', did='did323', role='admin', activated=0, password='Grave11.').save()
-        BMGTUser.objects.create(first_name='first321', last_name='last232', did='did232', role='user', activated=1, password='Grave11.').save()
+        BMGTUser(first_name='f', last_name='l', did='did', role='admin', activated=1, password='Grave11.').save()
+        BMGTUser(first_name='f', last_name='l', did='did323', role='admin', activated=0, password='Grave11.').save()
+        BMGTUser(first_name='first321', last_name='last232', did='did232', role='user', activated=1, password='Grave11.').save()
+        BMGTSemester(year=2022, season='fall').save()
+        BMGTGroup(number = 1, semester = BMGTSemester.objects.get(year=2022, season='fall')).save()
+
+    
+    def testGetGroupPositive(self):
+        c = Client()
+        _clientSignUp(c, 'did', 'Grave11.')
+        _clientSignIn(c, 'did', 'Grave11.')
+        _clientSetCookies(c, {'id': 1})
+        resp = c.get(
+            'bmgt435-service/api/groups?id=1',
+        )
+        self.assertEqual(resp.status_code, 200)
